8df0f41896473ceaaf4bccdc67216eeaf0ff3768
Cesar Romero <cra1982@gmail.com>
2020-08-08 11:49:20 -0500

    added language service

---
+++ src/app/core/services/language.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+export class LanguageService {
+
+  constructor(
+  ) {}
+ 
+  private getFirstBrowserLanguage(): string {
+      const nav: Navigator = window.navigator;
+      const browserLanguagePropertyKeys: any = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'];
+      let i = 0;
+      let language = '';
+
+      for (i = 0; i < browserLanguagePropertyKeys.length; i++) {
+          language = nav[browserLanguagePropertyKeys[i]];
+          if (language && language.length) {
+              return language;
+          }
+      }
+      return null;
+  }
+
+  public getCurrentLanguage(): string {
+      let language: string = this.getFirstBrowserLanguage() || 'en-US';
+      if (language.indexOf('-') !== -1) {
+          language = language.split('-')[0];
+      }
+      if (language.indexOf('_') !== -1) {
+          language = language.split('_')[0];
+      }
+      if (language === '') {
+          language = 'en';
+      }
+      return language;
+  }
+
+}
