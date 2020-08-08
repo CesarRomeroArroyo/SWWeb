7ec5aa8419c7120d851eb5d2f44a41911382490b
Cesar Romero <cra1982@gmail.com>
2020-08-08 11:45:11 -0500

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { CharactersComponent } from './pages/characters/characters.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    CharactersComponent
   ],
   imports: [
     BrowserModule,
